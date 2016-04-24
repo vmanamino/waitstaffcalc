b5468e93a4c7b0b61840f8ba219f77ff6d997c13
vmanamino <vmanamino@gmail.com>
2016-04-24 05:19:42 +0000

    customer charges added

--- js/app.js
+++ js/app.js
@@ -1,14 +1,29 @@
 angular.module('myApp', [])
     .controller('myCtrl', function($scope){
         $scope.basemeal = '';
+        $scope.taxrate = '';
+        $scope.tipPercentage = '';
+        $scope.tip = '';
+        $scope.subtotal = '';
+        $scope.total = '';
+        $scope.tiptotal = '';
+        $scope.meals = '';
+        $scope.avg = '';
+        $scope.displayErrors = 'no';
         $scope.submit = function(){
-            if ($scope.basemeal){
-                console.log($scope.basemeal);
+            if ($scope.basemeal && $scope.taxrate && $scope.tipPercentage){
+                $scope.subtotal = $scope.basemeal + ($scope.taxrate / 100);
+                $scope.tip = $scope.basemeal * ($scope.tipPercentage / 100);
+                $scope.total = $scope.subtotal + $scope.tip;
+            }
+            else {
+                $scope.displayErrors = '';
             }
         };
         $scope.addTwo = function(){
-            if ($scope.basemeal){
-                console.log($scope.basemeal + 2);
-            }
+            
+        };
+        $scope.reset = function(){
+            
         };
     })
