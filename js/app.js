bbce154083e0eeda987c39d7c7482b81bfa0946d
vmanamino <vmanamino@gmail.com>
2016-04-24 18:53:41 +0000

    fully functional

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ angular.module('myApp', [])
         $scope.basemeal = '';
         $scope.taxrate = '';
         $scope.tipPercentage = '';
-        $scope.tip = '';
+        $scope.tip =  0;
         $scope.subtotal = '';
         $scope.total = '';
-        $scope.tiptotal = '';
-        $scope.meals = '';
+        $scope.tiptotal = 0;
+        $scope.meals = 0;
         $scope.avg = '';
         $scope.displayErrors = 'no';
         $scope.submit = function(){
@@ -15,15 +15,30 @@ angular.module('myApp', [])
                 $scope.subtotal = $scope.basemeal + ($scope.taxrate / 100);
                 $scope.tip = $scope.basemeal * ($scope.tipPercentage / 100);
                 $scope.total = $scope.subtotal + $scope.tip;
+                $scope.addUp($scope.tip);
+                
             }
             else {
                 $scope.displayErrors = '';
             }
         };
-        $scope.addTwo = function(){
+        $scope.addUp = function(tip){
+            $scope.meals += 1;
+            $scope.tiptotal += tip;
+            $scope.avg = $scope.tiptotal / $scope.meals
+            
             
         };
         $scope.reset = function(){
-            
+            $scope.basemeal = '';
+            $scope.taxrate = '';
+            $scope.tipPercentage = '';
+            $scope.tip =  0;
+            $scope.subtotal = '';
+            $scope.total = '';
+            $scope.tiptotal = 0;
+            $scope.meals = 0;
+            $scope.avg = '';
+            $scope.displayErrors = 'no';
         };
     })
