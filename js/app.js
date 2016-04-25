7e7f7d5150a8ec633478a692b70c2e2ad13096e4
vmanamino <vmanamino@gmail.com>
2016-04-25 23:17:25 +0000

    added cancel

--- js/app.js
+++ js/app.js
@@ -10,19 +10,25 @@ angular.module('myApp', [])
         $scope.meals = 0;
         $scope.avg = '';
         $scope.displayErrors = 'no';
+        
         $scope.submit = function(){
             if ($scope.basemeal && $scope.taxrate && $scope.tipPercentage){
                 $scope.subtotal = $scope.basemeal + ($scope.taxrate / 100);
                 $scope.tip = $scope.basemeal * ($scope.tipPercentage / 100);
                 $scope.total = $scope.subtotal + $scope.tip;
-                $scope.addUp($scope.tip);
+                addUp($scope.tip);
                 
             }
             else {
                 $scope.displayErrors = '';
             }
         };
-        $scope.addUp = function(tip){
+        $scope.cancel = function(){
+            $scope.basemeal = '';
+            $scope.taxrate = '';
+            $scope.tipPercentage = '';
+        }
+        var addUp = function(tip){
             $scope.meals += 1;
             $scope.tiptotal += tip;
             $scope.avg = $scope.tiptotal / $scope.meals
